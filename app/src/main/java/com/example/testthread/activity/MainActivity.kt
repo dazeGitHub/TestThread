f0f805a6e1f54baa6013bde6eccb436215328336
WIN-QRE2IAFQ5E2\Administrator <zyz163mail@163.com>
2022-09-17 22:27:00 +0800

    again

--- app/src/main/java/com/example/testthread/activity/MainActivity.kt
+++ app/src/main/java/com/example/testthread/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.testthread
+package com.example.testthread.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testthread.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
